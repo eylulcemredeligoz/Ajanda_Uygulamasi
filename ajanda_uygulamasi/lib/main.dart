1a24b23f4304be1cb94fac9733c15d9b9fd5f036
SudeNazKol <sudekolnaz28@gmail.com>
2024-12-23 17:07:21 +0300

    firebase kuruldu

--- ajanda_uygulamasi/lib/main.dart
+++ ajanda_uygulamasi/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:ajanda_uygulamasi/sayfalar/ana_sayfa.dart';
 import 'package:ajanda_uygulamasi/sayfalar/oturum/giris.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MyApp());
+
+
+void main() async {
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
+runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
